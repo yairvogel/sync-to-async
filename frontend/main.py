c354dffc7339f4bb5d5dd93d5ab9363193d58729
Yair Vogel <yairvogel@gmail.com>
2025-09-06 13:32:50 +0200

    htmx messages

--- frontend/main.py
+++ frontend/main.py
@@ -1,15 +1,35 @@
-from fastapi import FastAPI
+from asyncio import sleep
+from fastapi import FastAPI, Request
 import fastapi
+from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi.templating import Jinja2Templates
 import httpx
 from urllib.parse import quote
 
 app = FastAPI()
+templates = Jinja2Templates("templates")
 
 
-@app.get("/{message}")
-async def ping(message: str):
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(request, "index.html")
+
+
+@app.get("/message")
+async def ping(request: Request, message: str):
     async with httpx.AsyncClient() as client:
         res: httpx.Response = await client.get(f"http://service:8080/{quote(message)}")
-        return fastapi.Response(
-            await res.aread(), res.status_code, media_type="text/plain"
+        return templates.TemplateResponse(
+            request, "response.html", {"message": res.text}
         )
+
+
+async def log_generator():
+    while True:
+        yield "event: ping\ndata: ping\n\n"
+        await sleep(1)
+
+
+@app.get("/log", response_class=StreamingResponse)
+async def log():
+    return StreamingResponse(log_generator(), media_type="text/event-stream")
